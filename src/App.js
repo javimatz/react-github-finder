f8cebe35943fbaebf6a83b74e463906d192a2cfe
javimatz <jvdelso@gmail.com>
2020-04-06 14:00:23 +0200

    add App class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>React App</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
