12739073d18110a5b12e0487c19a08a4a5288810
javimatz <jvdelso@gmail.com>
2020-04-07 17:05:16 +0200

    add users array mapping

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -4,27 +4,23 @@ class Users extends Component {
   state = {
     users: [
       {
-        [
-          {
-            id: 1,
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://github.com/mojombo',
-          },
-          {
-            id: 2,
-            login: 'defunkt',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-            html_url: 'https://github.com/defunkt',
-          },
-          {
-            id: 3,
-            login: 'pjhyett',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
-            html_url: 'https://github.com/pjhyett',
-          },
-        ]
-      }
+        id: 1,
+        login: 'mojombo',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+      {
+        id: 2,
+        login: 'defunkt',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+        html_url: 'https://github.com/defunkt',
+      },
+      {
+        id: 3,
+        login: 'pjhyett',
+        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+        html_url: 'https://github.com/pjhyett',
+      },
     ]
   }
   
@@ -32,6 +28,11 @@ class Users extends Component {
   render() {
     return (
   		<div>
+      {
+        this.state.users.map(
+          user => (<div>{user.login}</div>)
+        )
+      }
       </div>
     )
   }
