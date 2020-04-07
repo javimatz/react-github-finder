4c3ad5621b7a139d850a18fd98a0b74e2a2dbd0e
javimatz <jvdelso@gmail.com>
2020-04-07 13:50:57 +0200

    add UserItem state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'mojombo',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+      html_url: 'https://github.com/mojombo'
+    }
+  }
+
   render() {
     return (
   		<div>
