7d3c4d7ed6da9047145091a00d8d44855e34c38b
javimatz <jvdelso@gmail.com>
2020-04-07 13:09:50 +0200

    add UserItem component

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component {
+  
+  render() {
+    return (
+  		<div>
+        UserItem
+  		</div>
+    )
+  }
+}
+
+export default UserItem;
