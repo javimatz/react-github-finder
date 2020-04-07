85792dea32c3a2ff347c188fd3763542878e41fa
javimatz <jvdelso@gmail.com>
2020-04-07 12:37:26 +0200

    add defaultProps

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
 class Navbar extends Component {
+  
+  static defaultProps = {
+  	title: 'Github Finder',
+  	icon: 'fa fa-github'
+  };
+
   render() {
     return (
   		<nav className='navbar bg-primary'>
   			<h1>
-  				<i className='fab fa-github' /> Navbar
+  				<i className={this.props.icon} /> {this.props.title}
   			</h1>
   		</nav>
     )
