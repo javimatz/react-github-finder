baac413cd91e14385aa43bcdfa31d61fe8a32878
javimatz <jvdelso@gmail.com>
2020-04-07 19:22:56 +0200

    pass users array to UserItem component

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import UserItem from './UserItem';
 
 class Users extends Component {
   state = {
@@ -28,9 +29,7 @@ class Users extends Component {
     return (
   		<div>
       {
-        this.state.users.map(
-          user => (<div key={user.id}>{user.login}</div>)
-        )
+        this.state.users.map( user => ( <UserItem key={user.id} user={user} /> ) )
       }
       </div>
     )
