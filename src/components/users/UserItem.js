71343641061967b8b5278e20ae3aa3a61bb72935
javimatz <jvdelso@gmail.com>
2020-04-08 09:58:30 +0200

    refactor: UserItem stateless function

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
+const UserItem = (props) => {
 
-  render() {
-    const {login, avatar_url, html_url} = this.props.user;
+  const {login, avatar_url, html_url} = props.user;
 
-    return (
-  		<div className='card text-center'>
-        <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}} />
-  		  
-        <h3>{login}</h3>
+  return (
+		<div className='card text-center'>
+      <img src={avatar_url} alt="" className="round-img" style={{width: '60px'}} />
+		  
+      <h3>{login}</h3>
 
-        <div>
-          <a href={html_url} className='btn btn-dark btn-sm my-1' >
-          More
-          </a>
-        </div>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1' >
+        More
+        </a>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default UserItem;
