c73947799fa522ebf71e7f47c4553b6470ad9fdf
javimatz <jvdelso@gmail.com>
2020-04-08 16:34:22 +0200

    add: onChange function

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
+  state = {
+    text: '',
+  };
+  onChange = (e) => this.setState({ [e.target.name]: e.target.value });
+  
+
   render() {
     return (
       <form className="form">
@@ -8,6 +14,8 @@ class Search extends Component {
           type="text" 
           name="text" 
           placeholder="Search Users..." 
+          value={this.state.text}
+          onChange={this.onChange}
         />
         <input 
           type="submit" 
