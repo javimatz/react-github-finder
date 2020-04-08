9d526bd8d3352f029a811f02b4d7f8b4e4ce3e54
javimatz <jvdelso@gmail.com>
2020-04-08 15:54:24 +0200

    add: Search component

---
+++ src/components/users/Search.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+  render() {
+    return (
+      <form className="form">
+        <input 
+          type="text" 
+          name="text" 
+          placeholder="Search Users..." 
+        />
+        <input 
+          type="submit" 
+          value="Search" 
+          className="btn btn-dark btn-block" 
+        />
+      </form>
+    )
+  }
+}
+
+export default Search;
