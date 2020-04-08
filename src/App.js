ebcc07284c4485970be3190771f4a88ecdf6a80a
javimatz <jvdelso@gmail.com>
2020-04-08 11:25:16 +0200

    add: App state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
   async componentDidMount() {
+    this.setState({ loading: true });
+
     const res = await axios.get('https://api.github.com/users');
+    
+    this.setState({ users: res.data, loading: false });
   }
 
   render() {
