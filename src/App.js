c7575e5cd677fc6fceca0d5be86c594819bedb3d
javimatz <jvdelso@gmail.com>
2020-04-08 11:13:12 +0200

    add: Github Api request using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users')
+      .then(res => console.log(res.data));
+  }
+
   render() {
     return (
       <div className="App">
