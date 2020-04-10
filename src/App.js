cbe34535502184a0f041444970cdfb295538433e
javimatz <jvdelso@gmail.com>
2020-04-10 11:40:58 +0200

    add: About page component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -51,19 +52,23 @@ class App extends Component {
           <div className='container'>
             <Alert alert={this.state.alert} />
             <Switch>
-              <Route exact path='/' render={ props => (
-                <Fragment>
-                  <Search 
-                    searchUsers = {this.searchUsers} 
-                    clearUsers  = {this.clearUsers}
-                    showClear   = { users.length > 0 ? true : false }  
-                    setAlert    = {this.setAlert}
-                  />
-                  <Users loading={loading} users={users} />
-                </Fragment>
+              <Route 
+                exact 
+                path='/' 
+                render={ props => (
+                  <Fragment>
+                    <Search 
+                      searchUsers = {this.searchUsers} 
+                      clearUsers  = {this.clearUsers}
+                      showClear   = { users.length > 0 ? true : false }  
+                      setAlert    = {this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
               )}>  
               </Route>
             </Switch>
+
           </div>
         </div>
       </Router>
