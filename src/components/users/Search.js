8af572e77f373c2ecae2c3167bbf9f19945e0448
javimatz <jvdelso@gmail.com>
2020-04-14 11:40:23 +0200

    refactor: use useContext in Search component

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext'
 
-const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
+const Search = ({ showClear, clearUsers, setAlert }) => {
+
+  const githubContext = useContext(GithubContext);
 
   const [text, setText] = useState('');
 
@@ -11,7 +14,7 @@ const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
     if (text === '') {
       setAlert('Please enter something', 'light');
     } else {
-      searchUsers(text);
+      githubContext.searchUsers(text);
       setText('');
     }
   }
@@ -47,7 +50,6 @@ const Search = ({ showClear, clearUsers, setAlert, searchUsers }) => {
 }
 
 Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
   clearUsers: PropTypes.func.isRequired,
   showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired
