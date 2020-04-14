540755d6adde9071b20dbff41e8133e5e315533b
javimatz <jvdelso@gmail.com>
2020-04-14 17:28:59 +0200

    refactor: use AlertContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -8,59 +8,44 @@ import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 import './App.css';
 
 const App = () => {
-  
-  const [alert, setAlert] = useState(null);
-
-  // Search Github Users
-  
-  // Get single Github user
-
-  // Get user repos
-  
-  // Clear Users
-  
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-    setTimeout( () => setAlert(null), 5000 );
-  }
 
   return (
     <GithubState>
-      <Router>
-        <div className="App">
-          <Navbar />
-          { /* Container */ }
-          <div className='container'>
-            <Alert alert={alert} />
-            { /* Router Switch */ }
-            <Switch>
-              { /* Root route */ }
-              <Route 
-                exact 
-                path='/' 
-                render={ props => (
-                  <Fragment>
-                    <Search 
-                      setAlert    = {showAlert}
-                    />
-                    <Users />
-                  </Fragment>
-                )}
-              />  
-              { /* About page route */ }
-              <Route exact path='/about' component={About} />
-              { /* User profile route */ }
-              <Route exact path='/user/:login' component={User} />
-            </Switch>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            { /* Container */ }
+            <div className='container'>
+              <Alert />
+              { /* Router Switch */ }
+              <Switch>
+                { /* Root route */ }
+                <Route 
+                  exact 
+                  path='/' 
+                  render={ props => (
+                    <Fragment>
+                      <Search />
+                      <Users />
+                    </Fragment>
+                  )}
+                />  
+                { /* About page route */ }
+                <Route exact path='/about' component={About} />
+                { /* User profile route */ }
+                <Route exact path='/user/:login' component={User} />
+              </Switch>
+            </div>
+            { /* End container */ }
           </div>
-          { /* End container */ }
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   )  
 }
