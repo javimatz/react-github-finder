c67ab683d42783cbc01d98e8b80b25108671c49f
javimatz <jvdelso@gmail.com>
2020-04-15 11:21:37 +0200

    add: local enviroment variables

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -10,6 +10,17 @@ import {
 	GET_REPOS
 } from '../types';
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== 'production') {
+	githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+	githubClientId = process.env.GITHUB_CLIENT_ID;
+	githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
 	
 	const initialState = {
@@ -24,7 +35,8 @@ const GithubState = (props) => {
 	// Search Users
 	const searchUsers = async (text) => {
 	    setLoading();
-	    const res = await axios.get(`https://api.github.com/search/users?q=${text}`);
+	    const res = await axios.get(`https://api.github.com/search/users?q=${text}
+	    	&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 
 	    dispatch({
 	    	type: SEARCH_USERS,
@@ -35,7 +47,8 @@ const GithubState = (props) => {
 	// Get User
 	const getUser = async (username) => {
 		setLoading();
-		const res = await axios.get(`https://api.github.com/users/${username}`);
+		const res = await axios.get(`https://api.github.com/users/${username}
+			?client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 		
 		dispatch({
 			type: GET_USER,
@@ -46,7 +59,8 @@ const GithubState = (props) => {
 	// Get Repos
 	const getUserRepos = async (username) => {
 		setLoading();
-		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc`);
+		const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc
+			&client_id=${githubClientId}&client_secret=${githubClientSecret}`);
 
 		dispatch({
 			type: GET_REPOS,
