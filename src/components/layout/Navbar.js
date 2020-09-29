bbc140783e42c4c7c94fd45fb67f948d99a9ef2b
javimatz <jvdelso@gmail.com>
2020-09-29 20:13:15 +0200

    modify: navbar text color

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 
 const Navbar = ({title, icon}) => {
   return (
-		<div className='bg-dark'>
+		<div className='nav-container bg-dark'>
 			<nav className='navbar'>
-			<h1>
-				<i className={icon} /> {title}
-			</h1>
-			<ul>
-				<li>
-					<Link to='/'>Home</Link>
-				</li>
-				<li><Link to='/about'>About</Link></li>
-			</ul>
+				<h1>
+					<i className={icon} /> {title}
+				</h1>
+				<ul>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li><Link to='/about'>About</Link></li>
+				</ul>
 			</nav>
 		</div>
   )
